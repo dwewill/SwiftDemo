c4998baad55cd5318d53ff3f7c052f55c04f5a63
段文拯 <wenzhengduan1991@163.com>
2019-04-04 13:11:00 +0800

    添加密码的自动填充，以及webView跳转代理拦截

--- swiftDemo/swiftDemo/Classes/View/Main/OAuth/DWZOAuthViewController.swift
+++ swiftDemo/swiftDemo/Classes/View/Main/OAuth/DWZOAuthViewController.swift
@@ -17,6 +17,7 @@ class DWZOAuthViewController: UIViewController {
         view.backgroundColor = UIColor.white
         title = "登录新浪微博"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", fontSize: 14, action: self, selector: #selector(close), isBack: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "自动填充", action: self, selector: #selector(autoFill))
     }
     
     override func viewDidLoad() {
@@ -28,6 +29,7 @@ class DWZOAuthViewController: UIViewController {
         }
         let request = URLRequest(url: url)
         webView.loadRequest(request)
+        webView.delegate = self
     }
     
 
@@ -36,5 +38,22 @@ class DWZOAuthViewController: UIViewController {
 extension DWZOAuthViewController {
     @objc private func close() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func autoFill() {
+        let js = "document.getElementById('userId').value = '15889433219';"+"document.getElementById('passwd').value = 'dwz123456789';"
+        webView.stringByEvaluatingJavaScript(from: js)
+    }
+}
+
+
+// MARK: - UIWebViewDelegate
+extension DWZOAuthViewController: UIWebViewDelegate {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
+        if let res = request.url?.absoluteString.hasPrefix("http\\:www.baidu.com"), res == true {
+            print("授权成功 -- \(request.url?.absoluteString ?? "")")
+            return false
+        }
+        return true
     }
 }
